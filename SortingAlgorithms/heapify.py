3e3123c076307d81ca1a561f6a62a6aa8fe7216c
ahmedgozcelik <ahmetgozcelik@gmail.com>
2023-08-02 13:08:43 +0300

    heapify

---
+++ SortingAlgorithms/heapify.py
@@ -0,0 +1,27 @@
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 2
+    r = 2 * i + 2
+
+    if l < n and arr[largest] < arr[l]:
+        largest = l
+
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heapSort(arr):
+    n = len(arr)
+
+    #MAX-HEAP
+    for i in range(n, -1, -1):
+        heapify(arr, n, i)
+    
+    #SWAP
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+    return arr 
